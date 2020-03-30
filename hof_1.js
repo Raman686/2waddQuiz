2a3a4741aaf22732ac1107a5d73fbfe58801551d
Raman686 <61293616+Raman686@users.noreply.github.com>
2020-03-30 12:50:36 -0700

    hof_1.js

---
+++ hof_1.js
@@ -0,0 +1,44 @@
+
+
+
+//const persons = [
+//    { name: 'aman', age: 87},
+//    { name: 'raman', age: 15 },
+//    { name: 'amrit', age: 55 },
+//    { name: 'kamal', age: 95 },
+//    { name: 'karan', age: 5},
+//  ];
+//  const fullAge = persons.filter(person => person.age >= 18);
+//  console.log(fullAge);
+
+
+
+isEven = (num) => num % 2 ===0;
+result = [1, 2, 3, 4,5,6,7,8].filter(isEven);
+console.log(result);
+
+
+isOdd =(num)=> num  % 2 !== 0;
+result =[1,2,3,4,5,6,7,8].filter(isOdd);
+console.log(result); 
+
+isAbove3 =(num)=> num  >3;
+result =[1,2,3,4,5,6,7].filter(isAbove3);
+console.log(result); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
